02299edb9d0a248de6a46c9f476da3f48b9f074f
sferraro <sferraro@sfntb.lunatix.com.ar>
2017-12-17 13:41:30 -0300

    Cambio intervalo

--- build/cxx/cxx_exception_stubs.d
+++ build/cxx/cxx_exception_stubs.d
@@ -1,6 +1,6 @@
 cxx_exception_stubs.o: \
- /home/sferraro/esp32/esp-idf/components/cxx/./cxx_exception_stubs.cpp \
- /home/sferraro/esp32/esp-idf/examples/bluetooth/ble_ibeacon/build/include/sdkconfig.h
+ /home/sferraro/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp \
+ /home/sferraro/esp/Proyectos/ble_baliza/build/include/sdkconfig.h
 
-/home/sferraro/esp32/esp-idf/examples/bluetooth/ble_ibeacon/build/include/sdkconfig.h:
-/home/sferraro/esp32/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
+/home/sferraro/esp/Proyectos/ble_baliza/build/include/sdkconfig.h:
+/home/sferraro/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
--- main/ibeacon_demo.c
+++ main/ibeacon_demo.c
@@ -42,14 +42,14 @@ static esp_ble_scan_params_t ble_scan_params = {
     .scan_type              = BLE_SCAN_TYPE_ACTIVE,
     .own_addr_type          = BLE_ADDR_TYPE_PUBLIC,
     .scan_filter_policy     = BLE_SCAN_FILTER_ALLOW_ALL,
-    .scan_interval          = 0x50,
-    .scan_window            = 0x30
+    .scan_interval          = 0xA0,
+    .scan_window            = 0x90,
 };
 
 #elif (IBEACON_MODE == IBEACON_SENDER)
 static esp_ble_adv_params_t ble_adv_params = {
-    .adv_int_min        = 0x20,
-    .adv_int_max        = 0x40,
+    .adv_int_min        = 0xa0,
+    .adv_int_max        = 0x140,
     .adv_type           = ADV_TYPE_NONCONN_IND,
     .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
     .channel_map        = ADV_CHNL_ALL,
